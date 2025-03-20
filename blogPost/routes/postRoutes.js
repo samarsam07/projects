874df859b42f8ec14d44fac1d4be66071f2dc151
samar <samarmohd251@gmail.com>
2025-03-20 19:25:53 +0530

    add authentication middleware to post creation route

--- blogPost/routes/postRoutes.js
+++ blogPost/routes/postRoutes.js
@@ -2,8 +2,9 @@ const express=require('express');
 const postRouter=express.Router();
 const {getPostForm,createPost}=require('../controllers/postController');
 const upload=require('../config/multer');
+const {ensureAuthenticated}=require('../middleware/auth');
 
 postRouter.get("/add",getPostForm);
-postRouter.post("/add",upload.array("images",5),createPost);
+postRouter.post("/add",ensureAuthenticated,upload.array("images",5),createPost);
 
 module.exports=postRouter;
